170c72b351a747c993c8f4358a3f0609a12eb0a8
kay <duddnr654@naver.com>
2024-05-01 14:42:23 +0900

    chore: setting tailwindCSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import "./index.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
@@ -28,8 +29,9 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <h1 className="bg-slate-100 p-4 pt-2">TEST WORD</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
