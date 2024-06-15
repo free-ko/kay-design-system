4af2e16be940c63ae88d4d97c37e23060eafa9c8
kay <duddnr654@naver.com>
2024-06-15 16:05:59 +0900

    feat: add to 'color' constants file

---
+++ src/constants/themeAssets/colors.ts
@@ -0,0 +1,16 @@
+const COLORS = {
+  gray: {
+    200: "#F2F2F2",
+    300: "#E0E0E0",
+    400: "#646464",
+    600: "#5B5B5B",
+    800: "#3E3E3E",
+  },
+  white: "#ffffff",
+  black: "#000000",
+  orange: "#D9B23D",
+  primary: "#3367FF",
+  transparent: "transparent",
+} as const;
+
+export default COLORS;
