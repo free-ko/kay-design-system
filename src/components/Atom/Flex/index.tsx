026f98c3c4d23e457b42625449c465839ed7f075
kay <duddnr654@naver.com>
2024-06-17 07:22:29 +0900

    feat: add to 'theme'

--- src/components/Atom/Flex/index.tsx
+++ src/components/Atom/Flex/index.tsx
@@ -1,8 +1,7 @@
+import type { ElementType } from "react";
 import { cx } from "@emotion/css";
 
 import { flexDefaultStyle } from "./index.style";
-
-import type { ElementType } from "react";
 import type { FlexProps } from "./index.type";
 
 const Flex = <T extends ElementType = "div" | "nav" | "form">(
---
+++ src/constants/theme.ts
@@ -0,0 +1,11 @@
+import BREAK_POINTS from "./themeAssets/breakpoints";
+import TYPOGRAPHY from "./themeAssets/typography";
+import COLORS from "./themeAssets/colors";
+
+export const THEME = {
+  COLORS,
+  TYPOGRAPHY,
+  BREAK_POINTS,
+};
+
+export type Theme = typeof THEME;
